04b083f56891dbd5c988f6fd6901af47a7c51e28
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-04-25 23:28:35 +0530

    Add skills.py

---
+++ App/Skills.py
@@ -0,0 +1,19 @@
+data_science = ['Data Visualization','Predictive Analysis','Statistical Modeling',
+                      'Data Mining','Clustering & Classification','Data Analytics',
+                      'Quantitative Analysis','Web Scraping','Machine Learning','Deep Learning'
+                      ,'Keras','Pytorch','Probability','Scikit-learn','Tensorflow',"Flask",
+                      'Streamlit']
+
+web_development = ['React','Django','Node JS','React JS','php','laravel',
+                   'Magento','wordpress','Javascript','Angular JS','c#',
+                   'Flask','SDK']
+
+
+android_development = ['Android','Android development','Flutter','Kotlin',
+                        'XML','Java','Kivy','GIT','SDK','SQLite']
+
+
+ios_development = ['IOS','IOS Development','Swift','Cocoa','Cocoa Touch',
+                   'Xcode','Objective-C','SQLite','Plist','StoreKit',"UI-Kit",
+                   'AV Foundation','Auto-Layout']
+
